03e643e9022fa3f4222264136f7bc815a9aa0acc
Cokke <coke.jgomez@gmail.com>
2023-07-18 23:40:54 -0400

    Se cambia config para apuntar a BD en AWS

--- src/test/java/com/kibernumacademy/devops/DevopsApplicationTests.java
+++ src/test/java/com/kibernumacademy/devops/DevopsApplicationTests.java
@@ -20,21 +20,4 @@ class DevopsApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    void testRepositorySavesStudents() {
-        // given
-        Student student3 = new Student("Sofia", "Ramos", "sofia@gmail.com");
-        Student student4 = new Student("Catalina", "Marquez", "catalina@gmail.com");
-
-        // when
-        repository.save(student3);
-        repository.save(student4);
-
-        // then
-        Optional<Student> foundMiguel = repository.findById(student3.getId());
-        Optional<Student> foundCamila = repository.findById(student4.getId());
-
-        assertTrue(foundMiguel.isPresent());
-        assertTrue(foundCamila.isPresent());
-    }
 }
